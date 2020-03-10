40342de798d503ba266d84bb9a067609c6e6ad28
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-03-10 16:48:24 +0100

    initialisation de la methode tourne et redefinition de la methode avance

--- pglp_3_3/src/main/java/Abdelhafid/pglp_3_3/Rebot.java
+++ pglp_3_3/src/main/java/Abdelhafid/pglp_3_3/Rebot.java
@@ -1,5 +1,18 @@
 package Abdelhafid.pglp_3_3;
 
-public class Rebot {
+public class Rebot extends RebotStatique {
 
+	public Rebot(Position position, Direction direction) {
+		super(position, direction);
+		
+	}
+	
+	public void tourne() {
+		
+	}
+	
+	
+	@Override
+	public void avance(){}
+	
 }
